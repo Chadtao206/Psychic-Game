ef4f88470d507bf5d5958615c458f48115fa9aec
Chad Tao <chenchen408@gmail.com>
2018-06-29 11:17:31 -0700

    added final working version

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,12 +17,30 @@ document.onkeyup = function(event) {
     //Check if user guessed right.
     if (event.key == computerGuess) {
         win++;
+        alert("Awesome Job! I Know You Cheated By Looking In Console (-_^)")
+        document.querySelector("#answer").innerHTML = computerGuess;
         tries = 10;
         tried = [];
-        var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        var newcomputerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+        computerGuess = newcomputerGuess;
+        console.log("Computer Guessed " + computerGuess);
     }else{
         tries--
+        if (tries == 0){
+            lose++;
+            alert("You Suck! Maybe Don't Buy The Lottery Huh?")
+            document.querySelector("#answer").innerHTML = computerGuess;
+            tries = 10;
+            tried = [];
+            var newcomputerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+            computerGuess = newcomputerGuess;
+            console.log("Computer Guessed " + computerGuess);
+        }
     }
+    
     document.querySelector("#guess").innerHTML = tried;
     document.querySelector("#try").innerHTML = tries;
+    document.querySelector("#win").innerHTML = win;
+    document.querySelector("#lose").innerHTML = lose;
+
 }
